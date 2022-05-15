f91d70f50184be1b2ce711fb42af29ea14b552f3
lucaspallez <lucas.pallez@hotmail.com>
2022-05-15 22:55:01 +0200

    Added print to interface functionnality

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 
 //LoRa Pins
 #define LORA_FREQ         (868.0)
-#define LORA_SF           (9)
+#define LORA_SF           (7)
 
 // ======================================================== LoRa Commands ========================================================
 #define LORA_ARM              (0xA0)
@@ -30,26 +30,29 @@ RH_RF95 rf95(TE_CS_PIN, TE_INT_PIN);
 // ======================================================== Function Prototypes ========================================================
 void heartbeat(void);
 void LoRa_parse(uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]);
+void LoRa_parse_to_interface(uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]);
 
 // ======================================================== Setup ========================================================
 void setup() {
 
   //General Init
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(1000);
+
   pinMode(STATUS_LED_PIN, OUTPUT);
   pinMode(RX_LED_PIN, OUTPUT);
   pinMode(TX_LED_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
   // LoRa Init
-  while (!Serial) ; // Wait for serial port to be available
-  if (!rf95.init())
-    Serial.println("init failed"); 
-  Serial.println("Init Sucess!");
+  rf95.init();
+    //Serial.println("init failed"); 
+  //Serial.println("Init Sucess!");
   rf95.setTxPower(12, false);
   rf95.setFrequency(LORA_FREQ);
   rf95.setSpreadingFactor(LORA_SF);
 
+  digitalWrite(STATUS_LED_PIN, HIGH);
 }
 
 // ======================================================== Loop ========================================================
@@ -64,9 +67,11 @@ void loop()
     {
       digitalWrite(RX_LED_PIN, HIGH);
       //RH_RF95::printBuffer("request: ", buf, len);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-      LoRa_parse(buf);
+      //Serial.print("RSSI: ");
+      //Serial.println(rf95.lastRssi(), DEC);
+
+      //LoRa_parse(buf); // USE FOR PC DEBUG
+      LoRa_parse_to_interface(buf); //USE FOR USE WITH GRUND STATION
       digitalWrite(RX_LED_PIN, LOW);
     }
     else
@@ -74,8 +79,8 @@ void loop()
       Serial.println("Rx Error");
     }
   }
-  heartbeat();
-  delay(1000);
+  
+  //heartbeat();
 }
 
 // ======================================================== Functions ========================================================
@@ -130,7 +135,7 @@ void LoRa_parse(uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]){
 
   Serial.print(F("Pressure = "));
   memcpy(&float_buffer, &(buf[36]), sizeof(float));
-  Serial.print(float_buffer);
+  Serial.print(float_buffer/1000);
   Serial.println(" Pa");
 
   Serial.print(F("Approx altitude = "));
@@ -172,7 +177,7 @@ void LoRa_parse(uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]){
 
   Serial.print(F("GPS HDOP = "));
   memcpy(&float_buffer, &(buf[65]), sizeof(float));
-  Serial.print(float_buffer);
+  Serial.println(float_buffer);
 
   Serial.print(F("GPS Longitude = "));
   memcpy(&float_buffer, &(buf[69]), sizeof(float));
@@ -194,5 +199,106 @@ void LoRa_parse(uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]){
   Serial.print(float_buffer);
   Serial.println(" m/s");
 
+  Serial.print(F("RSSI = "));
+  memcpy(&float_buffer, &(buf[85]), sizeof(float));
+  Serial.print(float_buffer);
+
   Serial.println("");
+}
+
+void LoRa_parse_to_interface(uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]){
+
+  float float_buffer;
+
+  memcpy(&float_buffer, &(buf[0]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+  memcpy(&float_buffer, &(buf[4]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+  memcpy(&float_buffer, &(buf[8]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+  memcpy(&float_buffer, &(buf[12]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[16]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+  memcpy(&float_buffer, &(buf[20]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+  memcpy(&float_buffer, &(buf[24]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[28]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[32]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" "));
+
+  memcpy(&float_buffer, &(buf[36]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[40]), sizeof(float));
+  Serial.print(float_buffer); /* Adjusted to local forecast! */
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[44]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[48]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[52]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  Serial.print(buf[56]);
+  Serial.print(F(" ; "));
+
+  Serial.print(buf[57]);
+  Serial.print(F(" ; "));
+  Serial.print(buf[58]);
+  Serial.print(F(" ; "));
+  Serial.print(buf[59]);
+  Serial.print(F(" ; "));
+  Serial.print(buf[60]);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[61]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[65]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[69]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[73]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[77]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[81]), sizeof(float));
+  Serial.print(float_buffer);
+  Serial.print(F(" ; "));
+
+  memcpy(&float_buffer, &(buf[85]), sizeof(float));
+  Serial.print(float_buffer);
+
+  Serial.println(F(" \r"));
 }
